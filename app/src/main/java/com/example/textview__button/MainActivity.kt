c8167046c8c05322729b6d927d96ed5b1650e3c0
Sakender Saikot <sakendersaikot88@gmail.com>
2023-04-20 13:24:47 +0600

    Second upload

--- app/src/main/java/com/example/textview__button/MainActivity.kt
+++ app/src/main/java/com/example/textview__button/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.textview__button
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txv  : TextView
+    lateinit var txv2 : TextView
+    lateinit var txv3 : TextView
+    lateinit var txv4 : TextView
+
+
+    lateinit var btn  :  Button
+    lateinit var btn2 :  Button
+    lateinit var btn3 :  Button
+    lateinit var btn4 :  Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txv =  findViewById(R.id.tv)
+        txv2 = findViewById(R.id.tv2)
+        txv3 = findViewById(R.id.tv3)
+        txv4 = findViewById(R.id.tv4)
+
+
+        btn = findViewById(R.id.button)
+        btn2 = findViewById(R.id.button2)
+        btn3 = findViewById(R.id.button3)
+        btn4 = findViewById(R.id.button4)
+
+
+        btn.setOnClickListener {
+            txv.text = "Hi \uD83D\uDC4B, I'm Md.Sakender Saikot"
+
+
+        }
+
+
+
+
+
     }
 }
